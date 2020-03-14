240955134657dab1ea2495d7dabd0e15d25c9139
Alex Sánchez Pla <asanchez@ub.edu>
2020-03-14 21:49:34 +0100

    updated to include chapters 2 and 3

---
+++ canviaCaracters.R
@@ -0,0 +1,24 @@
+library(stringr)
+
+fesCanvis <- function (inputFile, outputFile){
+  x <- readLines(inputFile)
+  y1 <- str_replace_all(x, "\\\\'a", "á")
+  y2 <- str_replace_all(y1, "\\\\'e", "é")
+  y3 <- str_replace_all(y2, "\\\\'i", "í")
+  y4 <- str_replace_all(y3, "\\\\'o", "ó")
+  y5 <- str_replace_all(y4, "\\\\'u", "ú")
+  # y6 <- str_replace_all(y5, "\\\~n", "ñ")
+  
+  cat(y4, file=outputFile, sep="\n")
+}
+
+fesCanvis("capitulo5.tex", "02-Disenyo_de_Experiments.Rmd")
+
+##########################33
+
+x1 <- "cap\'itulo"
+x2 <- "\\section{Fuentes de variabilidad}"
+pattern <- "\\section\\{[[:alnum:][:blank:]]+\\}"
+replacement <-"\\s[[:alnum:][:blank:]]+"
+str_replace_all( x2, pattern, replacement)
+
